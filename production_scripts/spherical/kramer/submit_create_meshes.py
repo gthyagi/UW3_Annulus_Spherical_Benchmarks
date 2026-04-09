323ff295bd2f6510d1707ccf22fec35ef64a123b
Tyagi <gthyagi@gmail.com>
2026-04-09 15:31:55 +1000

    uncomment cellsizes

--- production_scripts/spherical/kramer/submit_create_meshes.py
+++ production_scripts/spherical/kramer/submit_create_meshes.py
@@ -6,8 +6,7 @@ import json
 from pathlib import Path
 import subprocess
 
-# CELL_SIZES = ["1/8", "1/16", "1/32", "1/64", "1/128"]
-CELL_SIZES = ["1/32"]
+CELL_SIZES = ["1/8", "1/16", "1/32", "1/64", "1/128"]
 CASES = [
     {"name": "smooth", "radius_inner": "1.22", "radius_internal": "None", "radius_outer": "2.22"},
     {"name": "delta", "radius_inner": "1.22", "radius_internal": "2.0", "radius_outer": "2.22"},
